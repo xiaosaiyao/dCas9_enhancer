0d2ff56d4bd09d6b80e8f54301d80d88e91cfc02
Xiaosai Yao <yao.xiaosai@gene.com>
2023-07-10 07:08:58 -0700

    add motif analysis

---
+++ script/homer.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#findMotifsGenome.pl ../results/O786.sig.bed hg19 ../results/homer/O786/ -size given
+#findMotifsGenome.pl ../results/A498.sig.bed hg19 ../results/homer/A498/ -size given
+findMotifsGenome.pl ../results/sig.enhancers.A498.786O.txt hg19 ../results/homer/both_2/ -size given
+
